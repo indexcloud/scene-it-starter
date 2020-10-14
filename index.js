1c7d401e763401f882e7a09a60455ce863fce310
Jason Deng <foralief@gmail.com>
2020-10-13 21:27:13 -0500

    up to part 1 step 2

---
+++ index.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    function renderMovies(movieArray) {
+        let movieHTML = `${movieArray.map(currentMovie => `
+            <div class="movie">
+            <img src="${currentMovie.Poster}"><img>
+            <h5>${currentMovie.Title}</h5>
+            <span>${currentMovie.Year}</span>
+            <button>Add!</button>
+            </div>
+        `).join("")}`;
+        return movieHTML;
+    }
+    document.querySelector(".movies-container").innerHTML = renderMovies(movieData);
+})
