77796b024d4e69afc2c876f5a476bdffdba0919a
Jason Deng <57022484+indexcloud@users.noreply.github.com>
2020-10-15 15:02:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
     function renderMovies(movieArray) {
         let movieHTML = `${movieArray.map(currentMovie => `
             <div class="movie">
-            <img src="${currentMovie.Poster}"><img>
-            <h5>${currentMovie.Title}</h5>
-            <span>${currentMovie.Year}</span>
-            <button>Add!</button>
+                <img src="${currentMovie.Poster}"><img>
+                <h5>${currentMovie.Title}<span>${currentMovie.Year}</span></h5>
+                <button type="button">Add!</button>
             </div>
         `).join("")}`;
         return movieHTML;
     }
     
-    document.getElementById(‘search-form’).addEventListener(‘submit’, function(e){
+    document.getElementById("search-form").addEventListener("submit", function(e){
         e.preventDefault();
         document.querySelector(".movies-container").innerHTML = renderMovies(movieData);
     });
