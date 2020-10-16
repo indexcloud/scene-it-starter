02eaee64164731835b1964577c4f27a5785e3bed
Jason <foralief@gmail.com>
2020-10-16 17:21:24 -0500

    finish part 3 and implement removeFromWatchlist function

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var movieSearchDatas; 
+
 document.addEventListener("DOMContentLoaded", function() {
     function renderMovies(movieArray) {
         return `${movieArray.map(currentMovie => `
@@ -16,11 +18,11 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log(searchString);
 
         var urlEncodedSearchString = encodeURIComponent(searchString);
-
         axios.get("http://www.omdbapi.com/?apikey=e4194de2&s=" + urlEncodedSearchString)
         .then(function(response) {
             console.log(response.data);
             var movieHTML = renderMovies(response.data.Search);
+            moviesSearchDatas = response.data.Search;
             document.querySelector(".movies-container").innerHTML = movieHTML;
 
         });
@@ -30,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function() {
 })
 
 function saveToWatchlist(imdbID) {
-    var movie = movieData.find(function(currentMovie) {
+    var movie = moviesSearchDatas.find(function(currentMovie) {
         return currentMovie.imdbID == imdbID;
     });
     var watchlistJSON = localStorage.getItem("watchlist");
--- watchlist.js
+++ watchlist.js
@@ -6,26 +6,27 @@ document.addEventListener("DOMContentLoaded", function() {
             <div class="movie">
                 <img src="${currentMovie.Poster}"><img>
                 <h5>${currentMovie.Title}<span>${currentMovie.Year}</span></h5>
-                <button type="button" onclick="unsaveToWatchlist('${currentMovie.imdbID}')">Remove!</button>
+                <button type="button" onclick="removeFromWatchlist('${currentMovie.imdbID}')">Remove!</button>
             </div>
         `).join("")}`;
     document.querySelector(".movies-container").innerHTML = movieHTML;
 });
 
-function unsaveToWatchlist(imdbID) {  // to remove movie from watchlist
-    var movie = movieData.find(function(currentMovie) {
-        return currentMovie.imdbID == imdbID;
-    });
+function removeFromWatchlist(imdbID) {  // to remove movie from watchlist
+   
     var watchlistJSON = localStorage.getItem("watchlist");
     var watchlist = JSON.parse(watchlistJSON);
-    if (watchlist === null) {
-        watchlist = [];
-    } 
-    console.log(movie);
-    console.log(typeof(watchlist));
-    var index = watchlist.indexOf(movie);
-    console.log(index);
-    watchlist.splice(index, 1);
+
+    var movie = watchlist.find(function(currentMovie) {
+        return currentMovie.imdbID == imdbID;
+    });
+
+    for (var i = 0; i < watchlist.length; i++) {
+        if (watchlist[i] === movie) {
+            watchlist.splice(i, 1);
+        };
+    }
+
     watchlistJSON = JSON.stringify(watchlist);
     localStorage.setItem("watchlist", watchlistJSON);
 };
